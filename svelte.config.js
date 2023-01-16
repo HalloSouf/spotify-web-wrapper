685d7eba59c4efac0a2160c8625aea03dbd474ab
HalloSouf <soufianesedd@gmail.com>
2023-01-16 19:58:22 +0100

    Added SvelteKit with Typescript and Vite

---
+++ svelte.config.js
@@ -0,0 +1,6 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+export default {
+  // More about preprocessors: https://svelte.dev/docs#compile-time-svelte-preprocess
+  preprocess: vitePreprocess(),
+}
