0e386fee4368653c0c5cb883e54fc6f7a8dab938
HalloSouf <soufianesedd@gmail.com>
2023-01-17 08:45:42 +0100

    Updated html file

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,4 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+// More about importing types and interfaces on https://kit.svelte.dev/docs/types#app
 declare global {
 	namespace App {
 		// interface Error {}
