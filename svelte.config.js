63c7773c32e58ee2bfabce4559134a5283a933da
HalloSouf <soufianesedd@gmail.com>
2023-01-17 15:13:11 +0100

    Added node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(), // More about preprocessors on https://kit.svelte.dev/docs/integrations#preprocessors
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ out: 'build' })
 	}
 };
 
