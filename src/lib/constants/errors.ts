cf210c8f1e3162ab52aade5e6c917ecc70e08a5f
HalloSouf <soufianesedd@gmail.com>
2023-02-09 09:42:41 +0100

    Escaped error message

--- src/lib/constants/errors.ts
+++ src/lib/constants/errors.ts
@@ -3,7 +3,7 @@ import type { IToastMessage } from '$ctypes/global.interface';
 const errors: Map<string, IToastMessage> = new Map<string, IToastMessage>();
 errors.set('unauthorized', {
 	title: 'Unauthorized',
-	message: 'Sorry, you're not authorized for this location...'
+	message: 'Sorry, you\'re not authorized for this location...'
 });
 
 export default errors;
